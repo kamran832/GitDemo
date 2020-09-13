4093671eb400b0ef787e478b6601f507d107ca7c
kamran832 <kamranb5rt@gmail.com>
2020-09-13 00:36:38 -0700

    add Testing.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -3,6 +3,6 @@ public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("Hello B20, How are you guys doing!");
 
-        System.out.println("I added so many codes");
+        System.out.println("I added so many codesccls");
     }
 }

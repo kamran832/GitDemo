788154ed6cb9cea0e7fb7bd3e64f260525756c11
kamran832 <kamranb5rt@gmail.com>
2020-09-12 23:20:12 -0700

    Update HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,5 +2,7 @@ public class HelloB20 {
 
     public static void main(String[] args) {
         System.out.println("Hello B20, How are you guys doing!");
+
+        System.out.println("I added so many codes");
     }
 }
